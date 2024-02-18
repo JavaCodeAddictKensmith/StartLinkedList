20af8031425933c67f5759a427fff009c985a19f
Kenneth.Ajah <kenneth.ajah@stanbicibtc.com>
2024-02-18 16:50:43 +0100

    feat

---
+++ LinkedListDSAImpl/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LinkedListDSAImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add("Test1");
+            list.Add("Test2");
+            list.Add(1,"Test3");
+            //for(int i =0; i<list.Count; i++)
+            //{
+            //    Console.WriteLine(list[KeyValuePair]);
+            //}
+
+            Console.WriteLine("Is it empty? -" + list.Empty);
+            Console.WriteLine("Is it empty? -" + list.Count);
+            //list.Remove(1);
+            //object test = list.Get(1);
+            //Console.WriteLine(test);
+
+            Console.WriteLine("After removal-" + list.Count);
+
+            Console.ReadLine();
+
+        }
+    }
+}
